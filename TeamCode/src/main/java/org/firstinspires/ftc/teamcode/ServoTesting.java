8926fef9a26bb0c253ad4bc6989da374756dc48e
Daniel-offshoreunk-web <offshore.unk@gmail.com>
2025-09-18 18:48:38 -0500

    Update ServoTesting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTesting.java
@@ -61,7 +61,7 @@ public class ServoTesting extends LinearOpMode {
 
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
 
         // Connect to servo (Assume Robot Left Hand)
         // Change the text in quotes to match any servo name on your robot.
@@ -73,12 +73,11 @@ public class ServoTesting extends LinearOpMode {
         telemetry.update();
         waitForStart();
 
-        while (!gamepad1.a) Thread.sleep(100);
         // Scan servo till stop pressed.
         while(opModeIsActive()){
 
             // slew the servo, according to the rampUp (direction) variable.
-            if (gamepad1.a) {
+            if (gamepad1.a || rampUp) {
                 // Keep stepping up until we hit the max value.
                 position += INCREMENT ;
                 if (position >= MAX_POS ) {
@@ -87,7 +86,7 @@ public class ServoTesting extends LinearOpMode {
                 }
             }
             }
-            if (gamepad1.b) {
+            if (gamepad1.b || !rampUp) {
                 // Keep stepping down until we hit the min value.
                 position -= INCREMENT ;
                 if (position <= MIN_POS ) {
