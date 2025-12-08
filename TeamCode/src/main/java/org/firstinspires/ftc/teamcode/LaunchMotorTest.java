9ff25e8d70cbe54cae6d3c9db752e11ed94aafed
rhsftc <ronsearle@hotmail.com>
2025-12-08 15:33:18 -0600

    Change LaunchMotorTest to OpMode, add intake motor toggling, and update controls
    - Refactor `LaunchMotorTest` from `LinearOpMode` to `OpMode` with standard lifecycle methods.
    - Rename OpMode to "Intake/Launch Motors Test".
    - Add functionality to toggle between controlling launch motors and intake motors using the 'A' button during init loop.
    - Change power control buttons from 'A'/'B' to D-pad Up/Down.
    - Update telemetry to show current motor selection and speed percentages.
    - Implement helper method `initMotors` to re-initialize motor groups dynamically.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaunchMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaunchMotorTest.java
@@ -1,42 +1,87 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
+import com.seattlesolvers.solverslib.gamepad.ToggleButtonReader;
 import com.seattlesolvers.solverslib.hardware.motors.Motor;
 import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
 import com.seattlesolvers.solverslib.hardware.motors.MotorGroup;
 
-@TeleOp(name = "Launch Motors Test", group = "test")
-public class LaunchMotorTest extends LinearOpMode {
-    MotorEx launchMotorLeft = null;
-    MotorEx launchMotorRight = null;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name = "Intake/Launch Motors Test", group = "test")
+public class LaunchMotorTest extends OpMode {
+    MotorEx leftMotor = null;
+    MotorEx rightMotor = null;
+    MotorGroup motorGroup = null;
+    // Default to launch motors at start
+    private String leftMotorName = "leftintake";
+    private String rightMotorName = "rightintake";
+    private boolean isLauncher = true;
     double newPower = 0;
     GamepadEx gamePadEx = new GamepadEx(gamepad1);
+    ToggleButtonReader aReader = new ToggleButtonReader(gamePadEx, GamepadKeys.Button.A);
 
-    public void runOpMode() {
-        telemetry.addLine("a increases power, b decreases power");
+    @Override
+    public void init() {
+        telemetry.addLine("a: Toggle launch/intake motors");
+        telemetry.addLine("press start to begin");
+        telemetry.addLine("dpad up: increase power, dpad down: decreases power");
         telemetry.update();
-        launchMotorLeft = hardwareMap.get(MotorEx.class, "leftlauncher");
-        launchMotorRight = hardwareMap.get(MotorEx.class, "rightlauncher");
-        MotorGroup launcherMotors = new MotorGroup(launchMotorLeft, launchMotorRight);
-        launcherMotors.setRunMode(Motor.RunMode.VelocityControl);
+        initMotors(leftMotorName, rightMotorName);
+    }
 
-        waitForStart();
-        while (opModeIsActive()) {
-            if (gamePadEx.wasJustReleased(GamepadKeys.Button.A)) {
-                newPower += 0.01;
-            }
-
-            if (gamepad1.b) {
-                newPower -= 0.01;
-            }
-
-            launcherMotors.set(newPower);
-            telemetry.addData("Left Power:", launcherMotors.getSpeeds().get(0));
-            telemetry.addData("Right Power:", launcherMotors.getSpeeds().get(1));
-            telemetry.update();
+    @Override
+    public void init_loop() {
+        aReader.readValue();
+        if (aReader.getState()) {
+            leftMotorName = "leftlauncher";
+            rightMotorName = "rightlauncher";
+            isLauncher = true;
+            initMotors(leftMotorName, rightMotorName);
+        } else {
+            leftMotorName = "leftintake";
+            rightMotorName = "rightintake";
+            isLauncher = false;
+            initMotors(leftMotorName, rightMotorName);
         }
+        telemetry.addData("Current Motors:", isLauncher ? "Launch Motors" : "Intake Motors");
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+        if (gamePadEx.getButton(GamepadKeys.Button.DPAD_UP)) {
+            newPower += 0.01;
+        }
+
+        if (gamePadEx.getButton(GamepadKeys.Button.DPAD_DOWN)) {
+            newPower -= 0.01;
+        }
+
+        motorGroup.set(newPower);
+        telemetry.addData("Left %:", motorGroup.getSpeeds().get(0));
+        telemetry.addData("Right %:", motorGroup.getSpeeds().get(1));
+        telemetry.update();
+    }
+
+    @Override
+    public void updateTelemetry(Telemetry telemetry) {
+        super.updateTelemetry(telemetry);
+    }
+
+    private void initMotors(String leftMotorName, String rightMotorName) {
+        leftMotor = hardwareMap.get(MotorEx.class, leftMotorName);
+        rightMotor = hardwareMap.get(MotorEx.class, rightMotorName);
+        motorGroup = new MotorGroup(leftMotor, rightMotor);
+        motorGroup.setRunMode(Motor.RunMode.VelocityControl);
     }
 }
