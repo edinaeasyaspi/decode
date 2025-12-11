e358d9025618129e4391add21dadbe9729746f96
rhsftc <ronsearle@hotmail.com>
2025-12-11 13:25:29 -0600

    Update Robot8034 OpMode description for clarity and relevance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot8034.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot8034.java
@@ -64,31 +64,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /*
- * This file contains an example of a Linear "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
- *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
- *
- * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
- *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
- * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
- * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ * This OpMode is the main teleOp for Decode.
  */
 @TeleOp(name = "Robot8034")
 public class Robot8034 extends LinearOpMode {
@@ -254,7 +230,7 @@ public class Robot8034 extends LinearOpMode {
  intakepower1 = true;
  it1null = false;
  }
- */
+*/
             // Send drive power to the wheels
             mecanumDrive.driveRobotCentric(isSlowMode ? gamePadEx.getLeftX() * SLOW_MODE_FACTOR : gamePadEx.getLeftX(),
                     isSlowMode ? gamePadEx.getLeftY() * SLOW_MODE_FACTOR : gamePadEx.getLeftY(),
