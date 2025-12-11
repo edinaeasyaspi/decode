e99db475933f9c7f22a695276ce7d7947772c511
rhsftc <ronsearle@hotmail.com>
2025-12-11 14:16:46 -0600

    Refactor LaunchManager to standardize variable naming and enhance code clarity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LaunchManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LaunchManager.java
@@ -4,19 +4,19 @@ import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
 import com.seattlesolvers.solverslib.hardware.motors.MotorGroup;
 
 public class LaunchManager {
-    public LaunchManager(MotorEx leftlaunchMotor, MotorEx rightlaunchMotor) {
-        this.leftLaunchMotor = leftlaunchMotor;
-        this.rightLaunchMotor = rightlaunchMotor;
+    public LaunchManager(MotorEx leftLaunchMotor, MotorEx rightLaunchMotor) {
+        this.leftLaunchMotor = leftLaunchMotor;
+        this.rightLaunchMotor = rightLaunchMotor;
         this.leftLaunchMotor.setInverted(true);
         this.rightLaunchMotor.setInverted(false);
 
-        this.launchMotors = new MotorGroup(leftlaunchMotor, rightlaunchMotor);
+        this.launchMotors = new MotorGroup(leftLaunchMotor, rightLaunchMotor);
         this.launchMotors.setRunMode(MotorEx.RunMode.VelocityControl);
     }
 
-    private MotorEx leftLaunchMotor;
-    private MotorEx rightLaunchMotor;
-    private MotorGroup launchMotors;
+    private final MotorEx leftLaunchMotor;
+    private final MotorEx rightLaunchMotor;
+    private final MotorGroup launchMotors;
 
     // Launch power allows a variable speed launch.
     public void launchOn(double launchPower) {
