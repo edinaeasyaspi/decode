bb88261e7c1929020a9a77d460a7ba41daa671b2
Daniel-offshoreunk-web <offshore.unk@gmail.com>
2026-01-16 15:38:09 -0600

    Manage retrival

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementPatternRetrieve.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+This class reads or writes AutonomousOptions objects to a file.
+ It is intended for use with the AutonomousConfiguration class.
+ */
+public class MovementPatternRetrieve {
+    private final String fileName = "movementPaths.txt";
+    private Context context;
+
+    public MovementPatternRetrieve(Context context) {
+        this.context = context;
+    }
+
+    // Check to see if options file exists.
+    public boolean optionsAreSaved() {
+        boolean result = true;
+        try {
+            context.openFileInput(fileName);
+        } catch (FileNotFoundException e) {
+            result = false;
+        }
+        return result;
+    }
+
+    public void storeObject(MovementPattern autonomousSpeeds) {
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(context.openFileOutput(fileName, Context.MODE_PRIVATE));
+            objectOutputStream.writeObject(autonomousSpeeds);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public MovementPattern getObject() {
+        ObjectInputStream objectInputStream;
+        MovementPattern autonomousSpeeds = null;
+        try {
+            FileInputStream fileInputStream = context.openFileInput(fileName);
+//            File filesDir = context.getFilesDir();
+//            String string = filesDir.toString();
+            objectInputStream = new ObjectInputStream(fileInputStream);
+            autonomousSpeeds = (MovementPattern) objectInputStream.readObject();
+            objectInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return autonomousSpeeds;
+    }
+}
