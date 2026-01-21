4251de3a234686756db08553cc3ae2f112fd2053
Daniel-offshoreunk-web <offshore.unk@gmail.com>
2026-01-20 20:37:06 -0600

    Check if it is a normal set

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ArtifactCellManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ArtifactCellManager.java
@@ -165,18 +165,22 @@ public class ArtifactCellManager {
         balls.add(centerCellColor);
         balls.add(rightCellColor);
         /// I know its ineffecient, but I know how to do this and this is simple
-        if ((balls.indexOf(CELL_COLOR.Green) == 0 && currentMotif == motif.GPP) || (balls.indexOf(CELL_COLOR.Green) == 1 && currentMotif == motif.PGP) || ((balls.indexOf(CELL_COLOR.Green) == 2 && currentMotif == motif.PPG))) {
-            launchOrder.set(0, CELL.Left);
-            launchOrder.set(1, CELL.Center);
-            launchOrder.set(2, CELL.Right);
-        } else if ((balls.indexOf(CELL_COLOR.Green) == 1 && currentMotif == motif.GPP) || (balls.indexOf(CELL_COLOR.Green) == 0 && currentMotif == motif.PGP) || (balls.indexOf(CELL_COLOR.Green) == 2 && currentMotif == motif.PPG)) {
-            launchOrder.set(0, CELL.Center);
-            launchOrder.set(1, CELL.Left);
-            launchOrder.set(2, CELL.Right);
-        } else if ((balls.indexOf(CELL_COLOR.Green) == 2 && currentMotif == motif.GPP) || (balls.indexOf(CELL_COLOR.Green) == 0 && currentMotif == motif.PGP) || (balls.indexOf(CELL_COLOR.Green) == 1 && currentMotif == motif.PPG)) {
-            launchOrder.set(0, CELL.Right);
-            launchOrder.set(1, CELL.Left);
-            launchOrder.set(2, CELL.Center);
+        if (Collections.frequency(balls, CELL_COLOR.Purple) == 2 && Collections.frequency(balls, CELL_COLOR.Green) == 1) {
+            if ((balls.indexOf(CELL_COLOR.Green) == 0 && currentMotif == motif.GPP) || (balls.indexOf(CELL_COLOR.Green) == 1 && currentMotif == motif.PGP) || ((balls.indexOf(CELL_COLOR.Green) == 2 && currentMotif == motif.PPG))) {
+                launchOrder.set(0, CELL.Left);
+                launchOrder.set(1, CELL.Center);
+                launchOrder.set(2, CELL.Right);
+            } else if ((balls.indexOf(CELL_COLOR.Green) == 1 && currentMotif == motif.GPP) || (balls.indexOf(CELL_COLOR.Green) == 0 && currentMotif == motif.PGP) || (balls.indexOf(CELL_COLOR.Green) == 2 && currentMotif == motif.PPG)) {
+                launchOrder.set(0, CELL.Center);
+                launchOrder.set(1, CELL.Left);
+                launchOrder.set(2, CELL.Right);
+            } else if ((balls.indexOf(CELL_COLOR.Green) == 2 && currentMotif == motif.GPP) || (balls.indexOf(CELL_COLOR.Green) == 0 && currentMotif == motif.PGP) || (balls.indexOf(CELL_COLOR.Green) == 1 && currentMotif == motif.PPG)) {
+                launchOrder.set(0, CELL.Right);
+                launchOrder.set(1, CELL.Left);
+                launchOrder.set(2, CELL.Center);
+            }
+        } else {
+            //For now just defualt to launching them all in order
         }
         return launchOrder;
     }
