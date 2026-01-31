15d54de5d280c50dfcbd90b353bb1fe83d14e937
rhsftc <ronsearle@hotmail.com>
2026-01-31 11:12:01 -0600

    Add motif to ApriltagManager

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/RevColorV3Manager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/RevColorV3Manager.java
@@ -17,9 +17,9 @@ public class RevColorV3Manager {
         float blue = colors.blue;
 
         // Simple threshold-based color detection
-        if (green > red && green > blue && green > 0.3) {
+        if (green > red && green > blue && green > 0.075) {
             return ArtifactCellManager.CELL_COLOR.Green;
-        } else if (green < red && green > blue && blue > 0.3) {
+        } else if (green > red && green< blue && blue > 0.07) {
             return ArtifactCellManager.CELL_COLOR.Purple;
         } else {
             return ArtifactCellManager.CELL_COLOR.None;
